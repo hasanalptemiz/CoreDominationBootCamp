e5b3f23170a103e27b8bcd56fda8d936c08700dc
Hasanalp <hasanalptemiz10@gmail.com>
2024-01-02 17:31:31 +0300

    program.cs update for task 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//Insert service to the container
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+                                             x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
+
+//Swagger config
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
 
 app.Run();
